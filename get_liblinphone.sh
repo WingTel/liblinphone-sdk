1eb63897de67f155d8451150e3adf07ce49ea253
Maxim Nikolaevich <speakus.net@gmail.com>
2017-03-22 12:58:40 +1000

    #210 update script for update

--- get_liblinphone.sh
+++ get_liblinphone.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+echo 'remove prev liblinphone'
+git clean -xdf
+echo 'download new liblinphone'
 curl -O "http://www.linphone.org/releases/ios/liblinphone-iphone-sdk-$1.zip"
 unzip "liblinphone-iphone-sdk-$1.zip"
+rm "liblinphone-iphone-sdk-$1.zip"
 mv ./liblinphone-sdk/apple-darwin ./
+
+echo 'please update liblinphone-sdk.podspec + create new tag + zip thi folder and download it to repository'
